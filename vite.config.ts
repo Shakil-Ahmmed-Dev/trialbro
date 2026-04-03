5b94baad503c4fe4a9c1730d533078ec7791a063
Shakil Ahmmed <shakilahmmedwakil.dev@gmail.com>
2026-04-04 02:36:36 +0600

    chore: update tsconfig and vite configuration
    - Removed baseUrl from tsconfig.json to streamline path resolution.
    - Added nitro plugin to vite.config.ts with Vercel preset for enhanced deployment support.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
+import { nitro } from 'nitro/vite'
+
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
@@ -10,6 +12,9 @@ import tailwindcss from '@tailwindcss/vite'
 const config = defineConfig({
   plugins: [
     devtools(),
+    nitro({
+      preset: 'vercel'
+    }),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
     tanstackStart(),
